dbcbe0d934071b798c7e6067aa5af3b87194b742
Andrey Morozov <and.morozov.v@gmail.com>
2022-12-26 13:58:17 +0300

    some change in estimate_person

--- hpe/estimate_person.py
+++ hpe/estimate_person.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 
+
 class PoseEstimator:
     def __init__(self, mode=False):
         self.LANDMARKS = ["LEFT_EAR", "RIGHT_EAR", "LEFT_SHOULDER",
@@ -12,12 +13,13 @@ class PoseEstimator:
         self.pose = self.mp_pose.Pose(
             static_image_mode=mode,
         )
-        self.points_posture = None
+        self.points_posture = dict()
 
     def process(self, name_image):
         img = cv2.imread(name_image)
         results = self.pose.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
         self.points_posture = self.points_selection(results)
+        self.points_posture["SCALE_IMG"] = (img.shape[0], img.shape[1])
 
     def points_selection(self, results):
         if not results.pose_landmarks:
@@ -37,4 +39,5 @@ if __name__ == "__main__":
     IMAGE_FILES = ["image1.png", "image2.jpg"]
     cls = PoseEstimator()
     res = cls.process(IMAGE_FILES[1])
+
     print(cls.points_posture)
