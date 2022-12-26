815b4206cdfabcad7020615e745b5e7dd086a068
Andrey Morozov <and.morozov.v@gmail.com>
2022-12-26 13:51:46 +0300

    added class estimate_person

--- hpe/estimate_person.py
+++ hpe/estimate_person.py
@@ -1,58 +1,40 @@
 import cv2
 import mediapipe as mp
-import numpy as np
-
-IMAGE_FILES = ["image1.png", "image2.jpg"]
-BG_COLOR = (192, 192, 192)
-
-mp_pose = mp.solutions.pose
-
-# recieved marked up image
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-
-
-with mp_pose.Pose(
-    static_image_mode=True,
-    model_complexity=2,
-    enable_segmentation=True,
-    min_detection_confidence=0.5,
-) as pose:
-    for idx, file in enumerate(IMAGE_FILES):
-        image = cv2.imread(file)
-
-        image_height, image_width, _ = image.shape
-        results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-
-        if not results.pose_landmarks:
-            continue
-
-        print(results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE])
-
-        annotated_image = image.copy()
-        condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.1
-        bg_image = np.zeros(image.shape, dtype=np.uint8)
-        bg_image[:] = BG_COLOR
-
-        annotated_image = np.where(condition, annotated_image, bg_image)
-
-        mp_drawing.draw_landmarks(
-            annotated_image,
-            results.pose_landmarks,
-            mp_pose.POSE_CONNECTIONS,
-            landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style()
-        )
-        cv2.imwrite(str(idx) + ".png", annotated_image)
-        # mp_drawing.plot_landmarks(results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
-
 
 class PoseEstimator:
-    def __init__(self):
-        pass
+    def __init__(self, mode=False):
+        self.LANDMARKS = ["LEFT_EAR", "RIGHT_EAR", "LEFT_SHOULDER",
+                          "RIGHT_SHOULDER", "RIGHT_HIP", "LEFT_HIP",
+                          "LEFT_KNEE", "RIGHT_KNEE", "RIGHT_ANKLE",
+                          "LEFT_ANKLE"]
 
-    def process(self):
-        pass
+        self.mp_pose = mp.solutions.pose
+        self.pose = self.mp_pose.Pose(
+            static_image_mode=mode,
+        )
+        self.points_posture = None
+
+    def process(self, name_image):
+        img = cv2.imread(name_image)
+        results = self.pose.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        self.points_posture = self.points_selection(results)
+
+    def points_selection(self, results):
+        if not results.pose_landmarks:
+            return None
+
+        posture = dict()
+
+        for LANDMARK in self.LANDMARKS:
+            point = results.pose_landmarks.landmark[self.mp_pose.PoseLandmark[LANDMARK]]
+            if point.visibility > 0.5:
+                posture[LANDMARK] = (point.x, point.y)
+
+        return posture
 
 
-
-
+if __name__ == "__main__":
+    IMAGE_FILES = ["image1.png", "image2.jpg"]
+    cls = PoseEstimator()
+    res = cls.process(IMAGE_FILES[1])
+    print(cls.points_posture)
