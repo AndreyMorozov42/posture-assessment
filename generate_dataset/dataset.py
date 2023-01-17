3074f7a70dfebd1b927537c7c5fada343a8b6f42
Andrey Morozov <and.morozov.v@gmail.com>
2023-01-17 11:38:13 +0300

    add class that generate dataset for neural network

---
+++ generate_dataset/dataset.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+class DataSet:
+    def __init__(self):
+        self.data = np.array([])
+
+    def build_data(self):
+        pass
+
+    def split(self, train, test):
+        pass
